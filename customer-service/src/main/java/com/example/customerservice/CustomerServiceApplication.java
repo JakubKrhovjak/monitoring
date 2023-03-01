9ea60a54eeef7b27234820fde25066ae149c2186
Jakub Krhovjak <jakub.krhovjak@protonmail.com>
2023-03-01 07:00:27 +0100

    basic

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,28 @@
 package com.example.customerservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @GetMapping("/something")
+    public ResponseEntity<String> createLogs() {
+        log.warn("Just checking");
+        return ResponseEntity.ok().body("All Ok");
     }
 
 }
